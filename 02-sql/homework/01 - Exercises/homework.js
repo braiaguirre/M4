94503a607f36f548adf6c1de652811e207588954
braiaguirre <brianaguirre.sp94@gmail.com>
2023-09-19 15:44:22 -0300

    update

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -1,16 +1,16 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = '';
+const ejercicio02 = `SELECT * FROM movies WHERE duration<90;`;
 
-const ejercicio03 = '';
+const ejercicio03 = `SELECT * FROM movies WHERE year>=1930 AND year<=1940;`;
 
-const ejercicio04 = '';
+const ejercicio04 = `SELECT * FROM movies WHERE title LIKE '%til%'`;
 
-const ejercicio05 = '';
+const ejercicio05 = `SELECT * FROM movies WHERE`;
 
-const ejercicio06 = '';
+const ejercicio06 = `SELECT * FROM movies WHERE`;
 
-const ejercicio07 = '';
+const ejercicio07 = `SELECT * FROM movies WHERE title LIKE '%Fast and%' and year=2016;`;
 
 module.exports = {
    ejercicio02,
