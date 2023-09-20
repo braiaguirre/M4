37a332431f3eb63d50f4c64c6bdadc38a8f92453
braiaguirre <brianaguirre.sp94@gmail.com>
2023-09-20 11:49:33 -0300

    update

--- 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
@@ -1,5 +1,19 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
-  sequelize.define('Ability', {});
+    sequelize.define('Ability', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: 'compositeIndex'
+        },
+        description: {
+            type: DataTypes.TEXT
+        },
+        mana_cost: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            unique: 'compositeIndex'
+        },
+    });
 };
--- 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Character.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Character.js
@@ -1,5 +1,34 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-   sequelize.define('Character', {});
+   sequelize.define('Character', {
+      code: {
+         type: DataTypes.INTEGER,
+         primaryKey: true,
+         validate: {
+            len: [5]
+         }
+      },
+      name: {
+         type: DataTypes.STRING,
+         unique: true
+      },
+      age: {
+         type: DataTypes.INTEGER
+      },
+      race: {
+         type: DataTypes.ENUM('Human', 'Elf', 'Machine', 'Demon', 'Animal', 'Other'),
+         defaultValue: 'Other'
+      },
+      hp: {
+         type: DataTypes.FLOAT,
+         allowNull: false
+      },
+      mana: {
+         type: DataTypes.FLOAT,
+         allowNull: false
+      }}, {
+         timestamps: false
+      }
+   );
 };
